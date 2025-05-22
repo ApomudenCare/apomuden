920f9fcd2d52518a7bb8d694d100c1cddee0ad08
kingsley <kingsleysomuah123@gmail.com>
2025-05-22 07:49:39 +0000

    first commit branch

--- src/components/constantfiles/NavBar.jsx
+++ src/components/constantfiles/NavBar.jsx
@@ -1,37 +1,38 @@
 import React from "react";
 import navigation from "../../jsfiles/navindex";
-import { BiGlobeAlt, BiWorld } from "react-icons/bi";
-import { GiWorld } from "react-icons/gi";
-import { FaInternetExplorer } from "react-icons/fa6";
 import { CiGlobe } from "react-icons/ci";
 import { TiWeatherSunny } from "react-icons/ti";
+import Dropdown from "../Dropdown";
+import Mode from "../Mode";
+import { Link } from "react-router";
 
 const NavBar = () => {
 	return (
-		<section className="border border-b-gray-300 p-4 flex justify-between items-center ">
+		<section className="border border-b-gray-300 p-4 flex justify-between items-center fixed top-0 right-0 left-0 bg-white/60 shadow-md backdrop-blur-md ">
 			<div className="flex gap-10 items-center">
-				<div>Apomuden Care</div>
+				<div className="text-[#3f9669] font-semibold">Apomuden Care</div>
 				<ul className="flex items-center gap-5">
 					{navigation.map((navitems, index) => (
-						<li key={index} className="flex items-center gap-2">
+						<Link
+							to={navitems.path}
+							key={index}
+							className="flex items-center gap-1 hover:text-[#3f9669] hover:bg-gray-100 p-2 cursor-pointer text-gray-600"
+						>
 							<navitems.icon />
 							{navitems.name}
-						</li>
+						</Link>
 					))}
 				</ul>
 			</div>
-			<div className="flex gap-5">
-				<div className="border border-gray-200 flex items-center px-3 py-1 gap-2 rounded-md">
-					<CiGlobe />
-					<p>Twi</p>
-				</div>
-				<div className="items-center flex"> 
+			<div className="flex gap-5 items-center">
+				<Dropdown />
+				<div className="items-center flex border border-gray-300 px-2 py-1 rounded-md">
 					<p>
-						<TiWeatherSunny />
+						<Mode />
 					</p>
 				</div>
-				<button>Emergency</button>
-				<p>ASK</p>
+				<button className="bg-red-500 text-white px-3 py-1 rounded-md">Emergency</button>
+				<p>KA</p>
 			</div>
 		</section>
 	);
--- src/jsfiles/navindex.js
+++ src/jsfiles/navindex.js
@@ -1,16 +1,18 @@
 import { CgDanger } from "react-icons/cg";
-import { CiHome, CiStethoscope } from "react-icons/ci";
-import { FaUsers } from "react-icons/fa";
+import { CiHome, CiStethoscope, CiUser } from "react-icons/ci";
+import { FaUser} from "react-icons/fa";
 
 const navigation = [
 	
 	{
 		name: "Home",
 		icon: CiHome,
+		path: '/'
 	},
 	{
 		name: "Patient",
-		icon: FaUsers,
+		icon: CiUser,
+		path: '/patient'
 	},
 	{
 		name: "HealthCare Worker",
